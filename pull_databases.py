074d210b0261ae458e309fe41e2537c1f679eb73
graceannobrien <67425562+graceannobrien@users.noreply.github.com>
2021-02-12 16:26:25 -0500

    Update pull_databases.py

--- pull_databases.py
+++ pull_databases.py
@@ -2,7 +2,7 @@
 ## Note: Check the links in the README file to ensure that the names/locations of these files have not changed. 
 import urllib
 
-# ClinVar database- change to GR38 if needed
+# ClinVar database- GR37 or GR38. 
 urllib.urlretrieve('ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz', 'clinvar_GR37.vcf.gz')
 
 # ALFA database
