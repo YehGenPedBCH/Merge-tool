0005a0d8390410f688f50ea0b2a3375a752c90b7
graceannobrien <67425562+graceannobrien@users.noreply.github.com>
2021-04-27 10:30:15 -0400

    Create clinvar_pull.sh

---
+++ clinvar_pull.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -c 1                    # Request one core
+#SBATCH -N 1                    # Request one node
+
+#SBATCH -t 0-12:00              # Run time D-HH:MM format
+#SBATCH -p short                # Partition to run in
+#SBATCH --mem=2000              # Memory total in MB (in all cores)
+#SBATCH -e /home/gao4/clinvar-master/src/error/hostname_%j.err  # File to which STDERR will be written, including jobID
+#SBATCH --mail-type=ALL         # Type of email notification- BEGIN, END, FAIL, ALL
+#SBATCH --mail-user=grace.obrien@childrens.harvard.edu  # Email where notifications send to
+
+# Loading required module
+#cd /home/gao4/clinvar-master/src
+module load gcc/6.2.0
+module load python
+
+
+# Gnomad pull and reformat
+# 1. Run pull script. This pulls both clinvar and gnomad
+python clinvar_pull.py
+
+# Add in date
+mv clinvar_GR37.vcf.gz clinvar_GR37_$(date +%F).vcf.gz
